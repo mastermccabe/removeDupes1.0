ef9fe55a2c7fff4d05764e58ba3a293d83923a42
McCabe <mccabe@wambl.com>
2018-03-20 12:27:40 -0700

    phase 1

---
+++ removeDuplicates.py
@@ -0,0 +1,22 @@
+
+def removeDups(immutableString):
+    tempString = []
+    for x in immutableString:
+        tempString.append(x)
+
+    tempString.sort()
+
+    returnList = []
+    newString = ''
+    for i in tempString:
+        if i not in returnList:
+            returnList.append(i)
+            newString = ''.join(returnList)
+
+
+    return newString
+
+
+
+string = "geeksforgeeks"
+print removeDups(string)
